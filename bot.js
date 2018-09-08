604d1d8100af4dee8393c3e94074b95d3d046288
jok999 <42048536+jok999@users.noreply.github.com>
2018-09-08 04:26:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,120 +19,16 @@ client.on('message', msg => {
 
 
 
-
-
-
-var moment = require ("moment")
-var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
-}
-//Tropy server codes
-client.on('guildMemberAdd', member => {
-	let welcomer = member.guild.channels.find("name","welcome");
-      if(!welcomer) return;
-      if(welcomer) {
-         moment.locale('ar-ly');
-         var h = member.user;
-        let norelden = new Discord.RichEmbed()
-        .setColor('#070000')
-        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
-         .addField(': تاريخ دخولك السيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true)      
-     welcomer.send({embed:norelden});          
-               
- 
-      }
-      
-const channel = member.guild.channels.find("name","welcome");
-if (member.user.bot) return;
-var Canvas = require("canvas-prebuilt")
-var jimp = require('jimp')
-  var currentTime = new Date(),
-hours = currentTime.getHours() + 3 ,
-minutes = currentTime.getMinutes(),
-seconds = currentTime.getSeconds();
-if (minutes < 10) {
-minutes = "0" + minutes;
-}
-var suffix = "AM";
-if (hours >= 12) {
-suffix = "PM";
-hours = hours - 12;
-}
-if (hours == 0) {
-hours = 12;
-}
-const w = ['./m.png'];
-              let Image = Canvas.Image,
-                  canvas = new Canvas(557, 241),
-                  ctx = canvas.getContext('2d');
-              fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-                  if (err) return console.log(err)
-                  let BG = Canvas.Image;
-                  let ground = new Image;
-                  ground.src = Background;
-                  ctx.drawImage(ground, 0, 0, 557, 241);
-
-      })
-                      let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
-                      jimp.read(url, (err, ava) => {
-                          if (err) return console.log(err);
-                          ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-                              if (err) return console.log(err);
-
-                                    ctx.font = '20px Arial Bold';
-                              ctx.fontSize = '10px';
-                              ctx.fillStyle = "#FFFFFF";
-                                ctx.fillText(member.user.username, 250, 135);
-
-
-                                              var guild;
-    while (!guild)
-                guild = member.guild
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
-                        member.guild.fetchInvites().then(invs => {
-      let user = Invite.inviter;
-      let invites = invs.filter(i => i.inviter.id === user.id);
-      let inviteCount = invites.reduce((p, v) => v.uses + p, 0);
-
- channel.send(`**Invited By** : **${Invite.inviter} (${inviteCount})**`);
- channel.send(`**WELCOME TO CJ Bot Support**:star: `);
-
-})
-}
-            dat[Inv] = Invite.uses;
-})
+client.on('ready', async() => {
+var server = "487425917648502797"; // ايدي السررفر
+var channel = "487425917648502801";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز **')
+    },305);
 })
 
 
-                              //AVATARً
-                              let Avatar = Canvas.Image;
-                              let ava = new Avatar;
-                              ava.src = buf;
-                              ctx.beginPath();
-                 ctx.arc(120.8, 120.5, 112.3, 0, Math.PI*2, true);
-                   ctx.closePath();
 
-                                 ctx.clip();
-
-                        ctx.drawImage(ava, 7, 8, 227, 225);
-                              ctx.closePath();
- channel.sendFile(canvas.toBuffer())
-                          })
-})
-});
-
-//tropy server codes
-
-
-
-
-const canvas = require("canvas-prebuilt")
 
 
 
