0ec88e81df33eba441ec2e23ae36c2d49e05776d
jok999 <42048536+jok999@users.noreply.github.com>
2018-09-15 00:49:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,23 @@ var channel = "487425917648502801";//ايدي الروم
 
 
 
-
+ client.on('ready', function(){	
+    var ms = 10000 ;	
+    var setGame = ['%help','%دعوه'];	
+    var i = -1;	
+    var j = 0;	
+    setInterval(function (){	
+        if( i == -1 ){	
+j = 1;	
+       }	
+        if( i == (setGame.length)-1 ){	
+            j = -1;	
+      }	
+       i = i+j;	
+              client.user.setGame(setGame[i],`https://www.twitch.tv/jokar_999`);
+}, ms);	
+	
+});
 
 
 
