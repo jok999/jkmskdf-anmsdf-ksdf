db5ca87029a626413ca6f7b0ca5aa1f8cb5c644d
jok999 <42048536+jok999@users.noreply.github.com>
2018-09-18 15:47:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ var channel = "487425917648502801";//ايدي الروم
 
  client.on('ready', function(){	
     var ms = 10000 ;	
-    var setGame = ['نوم','تصبحو على خير'];	
+    var setGame = ['يسعد مساكم','يسعد صباحكم'];	
     var i = -1;	
     var j = 0;	
     setInterval(function (){	
@@ -109,6 +109,194 @@ if(!args) return;
 message.channel.send(`** ${args}**`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
 }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+//نسخة جاكيو المطورة حقوق كودز فقط
+ client.on('message', message => { //jackeo جاكيو
+	 let prefix = "!!";
+    if (message.content.startsWith("تهكير")) {
+  if(!message.channel.guild) return message.reply(' ');//jackeo جاكيو
+      if (message.author.bot) return//jackeo جاكيو
+           message.delete();//jackeo جاكيو
+             let args = message.content.split(' ').slice(1);//jackeo جاكيو
+                   let virusname = args.join(' ');//jackeo جاكيو
+                 if (virusname < 1) {//jackeo جاكيو//jackeo جاكيو
+                     return message.channel.send("** رجائاََ منشن من تريد تهكيرة ** ");//jackeo جاكيو
+                                     }//jackeo جاكيو
+                 message.channel.send({embed: new Discord.RichEmbed().setTitle('Loading ' + virusname + "...").setColor(0xFF0000)}).then(function(m) {
+             setTimeout(function() {
+               m.edit({embed: new Discord.RichEmbed().setTitle(`** Loading  [▓] 1%**`).setColor(0xFF0000)})
+             }, 5500)//jackeo جاكيو
+             setTimeout(function() {
+                m.edit({embed: new Discord.RichEmbed().setTitle(`** Loading [▓▓▓▓] 25%**`).setColor(0xFF0000)})
+              }, 10500)//jackeo جاكيو
+              setTimeout(function() {
+                 m.edit({embed: new Discord.RichEmbed().setTitle(`** Loading [▓▓▓▓▓▓▓▓] 50%**`).setColor(0xFF0000)})
+               }, 15500)//jackeo جاكيو
+               setTimeout(function() {
+                  m.edit({embed: new Discord.RichEmbed().setTitle(`** Loading [▓▓▓▓▓▓▓▓▓▓▓▓▓▓] 75%**`).setColor(0xFF0000)})
+                }, 25500)//jackeo جاكيو
+           setTimeout(function() {
+               m.edit({embed: new Discord.RichEmbed().setTitle(`** Hacking Done [▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓] 100%**`).setColor(0xFF0000)})
+             }, 30500)//jackeo جاكيو
+                setTimeout(function() {
+               m.edit({embed: new Discord.RichEmbed().setTitle(`** ..يتم الدخول للحساب** `).setColor(0xFF0000)})
+             }, 40500)//jackeo جاكيو
+                setTimeout(function() {
+               m.edit({embed: new Discord.RichEmbed().setTitle(`** ..يتم حفض بينات الحساب** `).setColor(0xFF0000)})
+             }, 45500)//jackeo جاكيو
+                setTimeout(function() {
+               m.edit({embed: new Discord.RichEmbed().setTitle(`** ..يتم رفع البينات** `).setColor(0xFF0000)})
+             }, 50500)//jackeo جاكيو
+              setTimeout(function() {
+               m.delete()//jackeo جاكيو
+           }, 55000)//jackeo جاكيو
+             setTimeout(function() {
+               message.channel.send('** تم الاختراق  __Done Hacking__ **').then(msg => msg.delete(25000));
+           }, 60500)//jackeo جاكيو
+           });//jackeo جاكيو
+         }//jackeo جاكيو
+ });//jackeo جاكيو
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var fkk =[
+        {f:"بسم الله الرحمن الرحيم",k:"ب س م ا ل ل ه ا ل ر ح م ن ا ل ر ح ي م"},
+        {f:"باص",k:"ب ا ص"},
+        {f:"عربة",k:"ع ر ب ة"},
+        {f:"سيارة",k:"س ي ا ر ة"},
+        {f:"سيرفرنا احلى سيرفر",k:"س ي ر ف ر ن ا ا ح ل ى س ي ر ف ر"},
+        {f:"العنود",k:"ا ل ع ن و د"},
+        {f:"المستتكعكبتيه",k:"ا ل م س ت ت ك ع ك ب ت ي ه"},
+        {f:"دحوم",k:"د ح و م"},
+        {f:"اونرنا احلى اونر",k:"ا و ن ر ن ا ا ح ل ى ا و ن ر"},
+        {f:"الحياة حلوة",k:"ا ل ح ي ا ة ح ل و ة"},
+        {f:"كازخستان",k:"ك ا ز خ س ت ا ن"},
+        {f:"لحم الحمام حلال ولحم الحمار حرام ",k:"ل ح م ا ل ح م ا م ح ل ا ل و ل ح م ا ل ح م ا ر ح ر ا م"},
+        {f:"استونيا",k:"ا س ت و ن ي ا"},
+        {f:"وجغمه",k:"ل ق م ة و ج غ م ه"},
+        {f:"زنديق",k:"ز ن د ي ق"},
+        {f:"استراليا",k:"ا س ت ر ا ل ي ا"},
+        {f:"سوريا ",k:"س و ر ي ا"},
+        {f:"الاردن",k:"ا ل ا ر د ن"},
+        {f:"طماطم ",k:"ط م ا ط م"},
+        {f:"سارة",k:"س ا ر ة"},
+        {f:"دراجون",k:"د ر ا ج و ن"},
+        {f:"سيرفر",k:"س ي ر ف ر"},
+        {n:"الجبل",m:"ا ل ج ب ل"},
+        {n:"هضبة",m:"ه ض ب ة"},
+        {n:"خواطر",m:"خ و ا ط ر"},
+        {n:"ارحبو",m:"ا ر ح ب و"},
+        {n:"اطنخ سيرفر",m:"ا ط ن خ س ي ف ر"},
+        {n:"احبك",m:"ا ح ب ك"},
+        {n:"سبرايز",m:"س ب ر ا ي ز"},
+        {n:"ولي على أمتك",m:"و ل ي ع ل ى أ م ت ك"},
+        {n:"الو محد",m:"ا ل و م ح م د"},
+        {n:"جوكر",m:"ج و ك ر"},
+        {n:"كريزي",m:"ك ر ي ز ي"},
+	{n:"فلسطين",m:"ف ل س ط ي ن"},
+	{n:"مصر",m:"م ص ر"},
+	{n:"بحبك",m:"ب ح ب ك"},
+	
+	
+   ];
+
+
+   client.on("message", async message => {
+	   var prefix = "!!";
+    if(message.content == prefix+"فكك"){
+        if(UserBlocked.has(message.guild.id)) return message.channel.send("هناك جلسة .")
+        UserBlocked.add(message.guild.id)
+        var ask = fkk[Math.floor(Math.random() * fkk.length)];
+        let embed = new Discord.RichEmbed()
+        .setTitle('فكك كلمه')
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setColor("RANDOM")
+        .setDescription(ask.f);
+        message.channel.sendEmbed(embed).then(msg=> msg.delete(200000))
+        const msgs = await message.channel.awaitMessages(msg => msg.author.id !== client.user.id ,{maxMatches:1,time:100000});
+            UserBlocked.delete(message.guild.id)
+        msgs.forEach(result => {
+           if(result.author.id == client.user.id) return;
+           if(result.content == "فكك") return
+           if(result.content == ask.k){
+
+             let embeds = new Discord.RichEmbed()
+             .setTitle(':white_check_mark: اجابة صحيحة')
+             .setAuthor(message.author.username, message.author.avatarURL)
+             .setColor("RANDOM")
+             .setDescription(`**${result.author.username}** الإجابة صحيحة`);
+                message.channel.sendEmbed(embeds);                return;
+           } else {
+
+                               var embedx = new Discord.RichEmbed()
+             .setTitle(':x:خطاء')
+             .setAuthor(message.author.username, message.author.avatarURL)
+             .setColor("RANDOM")
+             .setDescription(`**${result.author.username}** الإجابة خاطئة`);
+
+                message.channel.sendEmbed(embedx);
+           }
+     });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
