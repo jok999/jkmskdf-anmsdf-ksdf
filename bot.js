422629dbf28874a63d5ba24b5c4d258e2b5a3888
jok999 <42048536+jok999@users.noreply.github.com>
2018-09-18 03:32:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,30 +22,23 @@ var channel = "487425917648502801";//ايدي الروم
 
 
 
- const devs = ['292042690470739968' , '310129588397277185' , '' , ''];
-const adminprefix = "%";
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/idk");
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-  }
-  });
-
+ client.on('ready', function(){	
+    var ms = 10000 ;	
+    var setGame = ['نوم','تصبحو على خير'];	
+    var i = -1;	
+    var j = 0;	
+    setInterval(function (){	
+        if( i == -1 ){	
+j = 1;	
+       }	
+        if( i == (setGame.length)-1 ){	
+            j = -1;	
+      }	
+       i = i+j;	
+              client.user.setGame(setGame[i],`https://www.twitch.tv/jokar_999`);
+}, ms);	
+	
+});
 
 
 
